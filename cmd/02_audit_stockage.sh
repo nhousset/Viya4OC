df2072b15f0e5ef82e4ce5f7a8a340f42e0e6bf3
nicolas <nicolas.housset@gmail.com>
2026-05-26 13:27:44 +0200

    fix path 02_audit_stockage.sh

--- cmd/02_audit_stockage.sh
+++ cmd/02_audit_stockage.sh
@@ -4,4 +4,4 @@ echo -e "\n=== [AUDIT DU STOCKAGE] ==="
 # On définit le namespace par défaut si ce n'est pas déjà fait
 export DEFAULT_NAMESPACE="${DEFAULT_NAMESPACE:-default}"
 # On exécute le programme Go qui prend le relais pour le reste
-./audit_stockage
+cmd/audit_stockage
